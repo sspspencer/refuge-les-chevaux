8a7fa61d2005ee2a2fc7d032923e1463fdebb9f5
Spencer Pichette <ssptuningperformance@gmail.com>
2023-05-22 17:40:10 -0400

    minor content update

--- src/Home.jsx
+++ src/Home.jsx
@@ -14,8 +14,8 @@ const Home = () => {
             Welcome to Refuges Les Chevaux D'Espoir!
           </h1>
           <p className="text-lg text-white mb-8">
-            We are a non-profit horse rescue dedicated to saving and
-            rehabilitating horses in need.
+            Make a horse's dreams come true, give it the hope of a second
+            chance!
           </p>
           <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
             Donate Now
@@ -25,10 +25,16 @@ const Home = () => {
       <section className="max-w-3xl mx-auto mt-8 text-center sm:text-start">
         <h2 className="text-2xl font-bold text-gray-800 mb-4">Our Mission</h2>
         <p className="text-lg text-gray-600 mb-8">
-          At Refuges Les Chevaux D'Espoir, we believe that every horse deserves
-          a loving home and a chance at a happy life. Our mission is to rescue,
-          rehabilitate, and rehome horses in need, and to provide education and
-          support to horse owners and enthusiasts.
+          Our rescue's mission is to save as many horses as possible. We take in
+          horses on their way to be slaughtered or horses with urgent medical
+          needs and that have been abandonned, in order to give them a second
+          chance. We rehabilitate them with the help of our veterinarian,
+          dentist, also using alternative medicine and foot care. Once they have
+          been rehabilitated, and only if their condition allows it, we adopt
+          them out to an approved family, under contract, and certain families
+          have rules to abide by. If the horse has many health issues, he will
+          spend the rest of his life in our fields and we commit to give him the
+          appropriate care for the rest of his life.
         </p>
         <h2 className="text-2xl font-bold text-gray-800 mb-4">
           How You Can Help
