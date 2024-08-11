900b0817b48f61e021a977ae0bc8bab552562a40
Spencer Pichette <ssptuningperformance@gmail.com>
2024-08-11 17:53:08 -0400

    fixed volunteer page on mobile phone

--- src/Vonlunteer.jsx
+++ src/Vonlunteer.jsx
@@ -25,9 +25,9 @@ const Volunteer = () => {
   return (
     <div className="relative isolate bg-white">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
-        <div className="relative px-6 pb-10 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
+        <div className="relative  px-4 pb-10 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
-            <div className="absolute inset-y-0 left-4 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2"></div>
+            <div className="absolute inset-y-0 left-0 sm:left-4 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2"></div>
             <h2 className="text-3xl font-bold tracking-tight text-gray-900">
               How can you become a volunteer?
             </h2>
@@ -37,7 +37,7 @@ const Volunteer = () => {
               Arcu sed malesuada et magna.
             </p>
           </div>
-          <div className=" flex  gap-6  sm:justify-start ">
+          <div className=" flex gap-2 sm:gap-6  sm:justify-start ">
             <div className="ml-auto w-44 flex-none space-y-6 pt-32  sm:pt-80 lg:order-none lg:pt-80 xl:order-none xl:pt-80">
               <div className="relative">
                 <img
@@ -66,7 +66,7 @@ const Volunteer = () => {
                 <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
               </div>
             </div>
-            <div className="w-44 flex-none space-y-8 pt-32 sm:pt-72 lg:pt-56">
+            <div className="w-44 flex-none space-y-8 pt-32 hidden sm:visible sm:pt-72 lg:pt-56">
               <div className="relative">
                 <img
                   alt=""
