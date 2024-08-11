3167ab7a384ad4106be8cd5c4eec52866e4a8af6
Spencer Pichette <ssptuningperformance@gmail.com>
2024-08-11 17:54:23 -0400

    fixed grey background in volunteer section

--- src/Vonlunteer.jsx
+++ src/Vonlunteer.jsx
@@ -27,7 +27,7 @@ const Volunteer = () => {
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
         <div className="relative  px-4 pb-10 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
-            <div className="absolute inset-y-0 left-0 sm:left-4 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2"></div>
+            <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2"></div>
             <h2 className="text-3xl font-bold tracking-tight text-gray-900">
               How can you become a volunteer?
             </h2>
